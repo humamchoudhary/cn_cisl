73a29a416930c078053e703df3400e6b7787ff1a
Muhammad Humam <humamchoudhary@gmail.com>
2024-04-26 23:12:53 +0500

    Update

--- main.go
+++ main.go
@@ -9,49 +9,42 @@ import (
 func main() {
 	// CREATING
 
-	t := models.Teacher{
-		Id:         5,
-		Name:       "John Doe",
-		Department: "Computer Science",
-	}
-	t.CreateTeacher()
-
-	// SELECTING by id
-	teacher := models.Teacher{Id: 4}
-	teacher.GetTeacherByID()
-	fmt.Println(teacher)
-
-	// Seletecting by name
-
-	// Single
-	teacher = models.Teacher{Name: "John Doe"}
-	teacher.GetTeacherByName()
-	fmt.Println(teacher)
-
-	// Multiple
-	var teachers []models.Teacher
-	teacher = models.Teacher{Name: "John Doe"}
-	err := teacher.GetTeacherByName(&teachers)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(teachers)
-
-	// router := gin.Default()
-	// t_r := router.Group("/teacher")
-	// {
-	// 	t_r.POST("/login", handler.HandlerTeacherLogin)
+	// t := models.Teacher{
+	// 	Id:         5,
+	// 	Name:       "John Doe",
+	// 	Department: "Computer Science",
 	// }
-	// router.LoadHTMLGlob("templates/*")
+	// t.CreateTeacher()
 
-	// // //router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
-	// // router.GET("/", func(c *gin.Context) {
-	// // 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-	// // 		"title": "Main website",
-	// // 	})
-	// // })
+	// // SELECTING by id
+	// teacher := models.Teacher{Id: 4}
+	// teacher.GetTeacherByID()
+	// fmt.Println(teacher)
 
-	// router.GET("/reserve", handler.GetreserveHandler)
-	// router.GET("/login", handler.GetLoginHandler)
-	// router.Run(":80")
+	// // Seletecting by name
+
+	// // Single
+	// teacher = models.Teacher{Name: "John Doe"}
+	// teacher.GetTeacherByName()
+	// fmt.Println(teacher)
+
+	// // Multiple
+	// var teachers []models.Teacher
+	// teacher = models.Teacher{Name: "John Doe"}
+	// err := teacher.GetTeacherByName(&teachers)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(teachers)
+
+	router := gin.Default()
+	t_r := router.Group("/teacher")
+	{
+		t_r.POST("/login", handler.HandlerTeacherLogin)
+	}
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/reserve", handler.GetreserveHandler)
+	router.GET("/login", handler.GetLoginHandler)
+	router.Run(":80")
 }

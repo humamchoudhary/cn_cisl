1c31b4c04decac925637b255246be76fc04b537d
Muhammad Humam <humamchoudhary@gmail.com>
2024-04-29 08:56:17 +0500

    reservation bug fixes

--- models/reserve.go
+++ models/reserve.go
@@ -15,18 +15,6 @@ type Reservation struct {
 	Recursive  bool   `json:"recursive" db:"recursive"`
 }
 
-func GetReservationByID(id int) (Reservation, error) {
-	var reservations []Reservation
-	err := database.Read("Reservation", map[string]interface{}{"id": id}, &reservations)
-	if err != nil {
-		return Reservation{}, err
-	}
-	if len(reservations) == 0 {
-		return Reservation{}, fmt.Errorf("reservation with ID %d not found", id)
-	}
-	return reservations[0], nil
-}
-
 func (reservation Reservation) CreateReservation() error {
 
 	err := database.Insert("Reservation", reservation)
@@ -36,15 +24,29 @@ func (reservation Reservation) CreateReservation() error {
 	return nil
 }
 
-func GetReservationsByRecurrence(recursive bool) []Reservation {
+func (r *Reservation) GetByID() error {
+	var reservations []Reservation
+	err := database.Read("Reservation", map[string]interface{}{"id": r.ID}, &reservations)
+	if err != nil {
+		return err
+	}
+	if len(reservations) == 0 {
+		return fmt.Errorf("reservation with ID %d not found", r.ID)
+	}
+	*r = reservations[0]
+	return nil
+}
+
+func GetReservationsByRecurrence(recursive bool) ([]Reservation, error) {
 	var reservations []Reservation
 	err := database.Read("Reservation", map[string]interface{}{"recursive": recursive}, &reservations)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return reservations
+	return reservations, nil
 }
-func GetReservationsByTime(startTime, endTime string) []Reservation {
+
+func GetReservationsByTime(startTime, endTime string) ([]Reservation, error) {
 	var reservations []Reservation
 	condition := map[string]interface{}{
 		"start_time": startTime,
@@ -52,48 +54,64 @@ func GetReservationsByTime(startTime, endTime string) []Reservation {
 	}
 	err := database.Read("Reservation", condition, &reservations)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return reservations
+	return reservations, nil
 }
 
-func GetReservationsByReserverID(reserverID string) []Reservation {
+func GetReservationsByReserverID(reserverID string) ([]Reservation, error) {
 	var reservations []Reservation
 	condition := map[string]interface{}{
 		"reserverId": reserverID,
 	}
 	err := database.Read("Reservation", condition, &reservations)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return reservations
+	return reservations, nil
 }
 
-func (reservation *Reservation) DeleteReservation() {
+func (r *Reservation) Delete() error {
 	condition := map[string]interface{}{
-		"id": reservation.ID,
+		"id": r.ID,
 	}
 	err := database.Delete("Reservation", condition)
 	if err != nil {
-		panic(err)
+		return err
 	}
+	return nil
 }
+func (r *Reservation) Edit(newReservation Reservation) error {
+	newReservationMap := make(map[string]interface{})
 
-func (reservation *Reservation) EditReservation(newReservation Reservation) error {
-	newReservationMap := map[string]interface{}{
-		"reserverId": newReservation.ReserverId,
-		"date":       newReservation.Date,
-		"start_time": newReservation.StartTime,
-		"end_time":   newReservation.EndTime,
-		"recursive":  newReservation.Recursive,
+	if newReservation.ReserverId != "" {
+		newReservationMap["reserverId"] = newReservation.ReserverId
+	}
+
+	if newReservation.Date != "" {
+		newReservationMap["date"] = newReservation.Date
+	}
+
+	if newReservation.StartTime != "" {
+		newReservationMap["start_time"] = newReservation.StartTime
+	}
+
+	if newReservation.EndTime != "" {
+		newReservationMap["end_time"] = newReservation.EndTime
+	}
+
+	if newReservation.Recursive != r.Recursive {
+		newReservationMap["recursive"] = newReservation.Recursive
 	}
 
 	condition := map[string]interface{}{
-		"id": reservation.ID,
+		"id": r.ID,
 	}
-	err := database.Update("Reservation", condition, newReservationMap)
+
+	err := database.Update("Reservation", newReservationMap, condition)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
